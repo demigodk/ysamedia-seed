74c7379d860f6e752456799fad9a7cb94842c962
unknown <bfkalombo@gmail.com>
2018-06-20 16:40:34 +0200

    Added functionality for seeding database with Admin user, also add user to Role table upon registration success

--- ysamedia/ysamedia/Data/UserRoleSeed.cs
+++ ysamedia/ysamedia/Data/UserRoleSeed.cs
@@ -2,16 +2,26 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
+using ysamedia.Models;
 
+/// <summary>
+/// @author     : Bondo Kalombo
+/// @date       : 20-06-2018
+/// </summary>
 namespace ysamedia.Data
 {
+    /// <summary>
+    /// This class is used to seed the database with 
+    /// Roles
+    /// It gets called in startup.cs
+    /// </summary>
     public static class UserRoleSeed
     {
         public static async Task InitializeAsync(ApplicationDbContext context, IServiceProvider serviceProvider)
         {
             // context.Database.EnsureCreated
             var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            string[] roleNames = { "Administrator", "Admin", "Member", "Guest" };
+            string[] roleNames = { "Administrator", "Admin", "Member", "Guest" };            
 
             IdentityResult roleResult;
 
@@ -23,7 +33,7 @@ namespace ysamedia.Data
                 {
                     roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
                 }
-            }           
+            }            
         }                    
     }
 }
