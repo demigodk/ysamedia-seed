3a746d51008466b55ff84aad9220ba36c50a9f12
unknown <bfkalombo@gmail.com>
2018-06-21 11:41:42 +0200

    Got date picker working

---
+++ ysamedia/ysamedia/Models/TimeLogViewModels/TimeLogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ysamedia.Models.TimeLogViewModels
+{
+    public class TimeLogViewModel
+    {
+        [Required(ErrorMessage = "Please select a date for your entry")]
+        [Display(Name = "Date")]
+        public string date { get; set; }
+
+        public int year { get; set; }
+
+        public int month { get; set; }
+
+        [Display(Name = "Reason")]
+        public string reason { get; set; }
+
+        [MaxLength(450)]
+        public string UserId { get; set; }
+    }
+}
