84b87e08e5ab08f71ca5fe00841610a92c7b77b9
unknown <bfkalombo@gmail.com>
2018-07-31 17:18:06 +0200

    Added functionality for setting, updating and displaying profile picture

---
+++ ysamedia/ysamedia/Controllers/PhotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ysamedia.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
