f279bd79ee0ee830d8bbda5b043719ae6b466293
BONDO KALOMBO <201315943@student.uj.ac.za>
2018-08-01 22:16:24 +0200

    Added Stats and Cards of Registered Users

--- ysamedia/ysamedia/Controllers/HomeController.cs
+++ ysamedia/ysamedia/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ysamedia.Entities;
 using ysamedia.Models;
 
 namespace ysamedia.Controllers
@@ -8,11 +13,77 @@ namespace ysamedia.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+
+        private readonly ysamediaDbContext _context;
+
+        public HomeController(ysamediaDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<User> UserList = new List<User>();
+
+            UserList = (from u in _context.User
+                        select u).ToList();
+
+            ViewData["NumMediaMembers"] = UserList.Count;
+
+            List<Log> SundaysCountList = new List<Log>();
+
+            SundaysCountList = (from l in _context.Log
+                                select l).ToList();
+
+            int sundayCount = SundaysCountList.Select(x => x.Date).Distinct().Count();
+
+            ViewData["NumSundays"] = sundayCount;           
+
+            List<Log> LogList = new List<Log>();
+
+            LogList = (from l in _context.Log
+                       select l).ToList();
+           
+            ViewData["NumLogs"] = LogList.Count;
+
+            List<ChurchMember> churchMembersList = new List<ChurchMember>();
+
+            churchMembersList = (from c in _context.ChurchMember
+                                 select c).ToList();
+
+            ViewData["NumChurchMembers"] = churchMembersList.Count;
+           
+            var ysamediaDbContext = _context.User.Include(c => c.Gender);            
+
+            return View(ysamediaDbContext.ToList());
         }
-       
+
+        // GET: ChurchMembers/Details/5
+        public async Task<IActionResult> Profile(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaMember = await _context.User
+                .Include(m => m.Gender)
+                .Include(m => m.AttributeUserBridge)
+                .Include(m => m.DlicenceUserBridge)
+                .Include(m => m.NegAttributeUserBridge)
+                .Include(m => m.ScreeningAnswer)
+                .Include(m => m.Photo)
+                .Include(m => m.RateAnswerUserBridge)
+                .SingleOrDefaultAsync(m => m.UserId == id);
+
+            if (mediaMember == null)
+            {
+                return NotFound();
+            }            
+
+            return View(mediaMember);
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
